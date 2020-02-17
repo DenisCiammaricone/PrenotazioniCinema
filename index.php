0520275cc36903d78428b4ae4edc604279954207
Denis Ciammaricone <dakrin01@outlook.it>
2020-02-17 18:26:03 +0100

    First Upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="it">
+    <head>
+        <title>Cinema *name*</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        
+        <link href="inc/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <script src="inc/js/bootstrap.min.js" type="text/javascript"></script>
+        
+        <link href="inc/css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    
+    <body class="bg-light">
+        
+        <aside class="navbar navbar-dark">
+            <div class="bg-danger">
+                <button class="navbar-toggler no-decoration" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon no-decoration"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav bg-danger pt-2 pb-4 pl-4 pr-2">
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Roba</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Altro</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Assistenza</a></li>
+                    </ul>
+                </div>
+            </div>
+        </aside>
+        
+        <header class="jumbotron container text-center bg-dark text-light">
+            <h1 class="display-2">Benvenuti al cinema *name*</h1>
+        </header>
+        
+        
+        
+    </body>
+</html>
