14c6f18470bdf588cb85c733f9b2fd73a923b476
DeathDragon-html <61159386+DeathDragon-html@users.noreply.github.com>
2020-02-17 21:34:34 +0100

    Sempre footer

--- index.php
+++ index.php
@@ -231,8 +231,41 @@
             </section>
         </main>
         
-        <footer class="text-light">
-            sos
+        <footer class="text-light pt-2 pr-4">
+            <div class="card-deck text-center">
+                
+                <div class="card dark-footer" style="width:20vw;">
+
+                  <div class="card-body">
+                      <p class="card-text">CopyRight @2019</p>
+                  </div>
+
+                </div>
+                
+                <div class="card dark-footer" style="width:20vw;">
+
+                          <div class="card-body">
+                              <p class="card-text">Giusto per prova</p>
+                          </div>
+
+                </div>
+                
+                <div class="card dark-footer" style="width:20vw;">
+
+                          <div class="card-body">
+                              <p class="card-text"><b>Made by</b><br>
+                                    <i>Ciammaricone Denis</i><br>
+                                    <i>Di Simone Andrea</i><br>
+                                    <i>Lotorio Luca</i><br>
+                                    <i>Stortini Corrado</i><br>
+                                    <i>Tupitti Leonardo</i><br>
+                                    <i>Vetuschi Luigi</i>
+                              </p>
+                          </div>
+
+                </div>
+                
+            </div>
         </footer>
         
     </body>
