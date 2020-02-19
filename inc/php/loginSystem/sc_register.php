0322f961150d097da999f80021740e6f1bb83cec
Denis Ciammaricone <dakrin01@outlook.it>
2020-02-19 20:18:47 +0100

    Added Database Functionalyties

--- inc/php/loginSystem/sc_register.php
+++ inc/php/loginSystem/sc_register.php
@@ -1,8 +1,15 @@
 <?php
     require_once("../db_connection.php");
 
-    $fields = array('name','surname','email','pass', 'confirmTerms', 'confirmTerms');
+    $fields = array('name','surname','email','pass', 'confirmPass', 'confirmTerms');
+    
+    //Check for special characters
+    $name = mysqli_real_escape_string($conn,$_POST['name']);
+    $surname = mysqli_real_escape_string($conn,$_POST['surname']);
+    $email = mysqli_real_escape_string($conn,$_POST['email']);
+    $pass = mysqli_real_escape_string($conn,$_POST['pass']);
 
+    //Check for empty fields
     $emptyFieldDetected= false;
     foreach($fields as $needed){
         if(empty($_POST[$needed])){
@@ -10,6 +17,7 @@
         }
     }
 
+    //Output error in case of empty fields
     if($emptyFieldDetected){
         echo('
             <script type="text/javascript">
@@ -20,6 +28,10 @@
         exit();
     }
 
-    
+    //Query to add a ner user as a record
+    $q = "INSERT INTO users ('name','username','email','pass')
+                     VALUES ('$name','$surname','$email','$pass')";
+    $result = mysqli_query($conn, $q);
 
+    mysqli_close($conn);
 ?>
