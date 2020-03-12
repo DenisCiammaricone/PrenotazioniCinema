6fffd85284dcd313870b741a1a6af66789e559ef
Denis Ciammaricone <Dakrin28@gmail.com>
2020-03-12 10:25:40 +0100

    a

--- admin/php/sc_addFilm.php
+++ admin/php/sc_addFilm.php
@@ -19,10 +19,39 @@
     }
 
     //PRendi i codici del Genere e del Nome e Cognome del Regista
-
-    //Query per caricamento in Elenco Film
-    $q = "INSERT INTO Elenco_Film";
+    $q = "SELECT PK_CodG FROM GeneriFilm WHERE Nome = $genere";
     $result = mysqli_query($conn, $q);
+
+    $codiceGenere = -1;
+    $codiceRegista = -1;
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_array($result)){
+            $codiceGenere = $row['PK_CodG'];
+        }
+        if($codiceGenere >= 0){
+            $q = "SELECT PK_CodR FROM RegistiFilm WHERE Nome = $nomeReg AND Cognome = $cognReg";
+            $result = mysqli_query($conn, $q);
+            if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_array($result)){
+                    $codiceRegista = $row['PK_CodR'];
+                }
+                if($codiceRegista >= 0){
+                    //Query per caricamento in Elenco Film
+                    $q = "INSERT INTO Elenco_Film";
+                    $result = mysqli_query($conn, $q);
+                }
+            }
+
+        } else {
+            echo('
+                <script>alert("Nessun genere con questo nome è stato trovato");</script>
+            ');
+        }
+    } else {
+        echo('
+            <script>alert("Nessun genere con questo nome è stato trovato");</script>
+        ');
+    }
     mysqli_close($conn);
     
 ?>
